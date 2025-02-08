ae422deedc5fa024277e7f3f356b417743873bc9
SaranyaY1320 <saranya.y1320@gmail.com>
2025-02-08 18:13:52 -0500

    Delete model directory

--- model/users.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type User struct {
-	ID       uint   `json:"id" gorm:"primaryKey"`
-	Username string `json:"username" gorm:"not null;column:username;size:255"`
-	Email    string `json:"email" gorm:"not null;column:email;size:255"`
-	Password string `json:"password" gorm:"not null;column:password;size:255"`
-}
